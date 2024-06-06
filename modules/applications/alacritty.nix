12be708c6beb70bfbd3067b678ef24908852eb00
Kim Goetzke <kbgoetzke@gmail.com>
2024-06-06 09:56:13 +0100

    WIP - Untested Alacritty changes

--- modules/applications/alacritty.nix
+++ modules/applications/alacritty.nix
@@ -13,7 +13,41 @@ in {
     programs.alacritty = {
       enable = true;
       settings = {
-        window.opacity = lib.mkForce 0.75;
+        window = {
+          opacity = lib.mkForce 0.75;
+          startup_mode = "Maximized";
+          decorations = "None";
+          dynamic_title = true;
+        };
+        live_config_reload = true;
+        shell.program = "${pkgs.bash}/bin/bash";
+        mouse.hide_while_typing = true;
+        selection.save_to_clipboard = true;
+        colors = {
+          primary = {
+            background = "#2E3440";
+            foreground = "#D8DEE9";
+          };
+          normal = {
+            black   = "#3B4252";
+            red     = "#BF616A";
+            green   = "#A3BE8C";
+            yellow  = "#EBCB8B";
+            blue    = "#81A1C1";
+            magenta = "#B48EAD";
+            cyan    = "#88C0D0";
+            white   = "#E5E9F0";
+          };
+          bright = {
+            black   = "#4C566A";
+            red     = "#BF616A";
+            green   = "#A3BE8C";
+            yellow  = "#EBCB8B";
+            blue    = "#81A1C1";
+            magenta = "#B48EAD";
+            cyan    = "#8FBCBB";
+            white   = "#ECEFF4";
+          };
       };
     };
   };
