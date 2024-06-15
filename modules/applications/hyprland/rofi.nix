c75063f4f923a863726abb74099a6aa8ed849d0d
Kim Goetzke <kbgoetzke@gmail.com>
2024-06-15 18:54:20 +0100

    Style rofi

--- modules/applications/hyprland/rofi.nix
+++ modules/applications/hyprland/rofi.nix
@@ -13,27 +13,24 @@
     programs.rofi = {
       enable = true;
       package = config.rofi.package;
-#      theme = with config.lib.stylix.colors.withHashtag lib.mkForce;
-#        builtins.toFile "theme.rasi" ''
-#          * {
-#              font:   "JetBrainsMono Nerd Font Regular 15";
-#
-#              bg0:     ${base01};
-#              bg1:     ${base02};
-#              fg0:     ${base04};
-#
-#              accent-color:     ${base03};
-#              urgent-color:     #ffffff;
-#
-#              background-color:   transparent;
-#              text-color:         @fg0;
-#
-#              margin:     0;
-#              padding:    0;
-#              spacing:    0;
-#          }
-#
-#          ${builtins.readFile ./../../../assets/configs/hyprland/rofi-theme.rasi}'';
+      theme = lib.mkForce (with config.lib.stylix.colors.withHashtag;
+        builtins.toFile "theme.rasi" ''
+          * {
+              font:               "DejaVu Sans Regular 15";
+              bg0:                ${base00};
+              bg1:                ${base02};
+              fg0:                ${base03};
+              fg1:                ${base0D};
+              fg2:                ${base0A};
+              accent-color:       ${base05};
+              urgent-color:       #ffffff;
+              background-color:   transparent;
+              text-color:         @fg0;
+              margin:             0;
+              padding:            0;
+              spacing:            0;
+          }
+          ${builtins.readFile ./../../../assets/configs/hyprland/rofi-theme.rasi}'');
       cycle = true;
       plugins = with pkgs; [
         rofi-emoji
