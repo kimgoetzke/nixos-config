538e41f727619b71883657c1c3d12fc970b9a9b6
Kim Goetzke <kbgoetzke@gmail.com>
2024-06-24 11:58:11 +0100

    Update fo.sh

--- assets/scripts/fo.sh
+++ assets/scripts/fo.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+# shellcheck disable=SC2164
 
 fo() {
   if [[ -z $NIX_USER || ${#NIX_USER} -lt 2 ]]; then
@@ -8,20 +9,21 @@ fo() {
 
   case "$1" in
   "pro")
-    cd "/home/$NIX_USER/projects/" || exit
+    cd "/home/$NIX_USER/projects/"
     ;;
   "home" | "kim")
-    cd "/home/$NIX_USER/" || exit
+    cd "/home/$NIX_USER/"
     ;;
   "nix" | "nixr")
-    cd "/home/$NIX_USER/projects/nixos-config/" || exit
+    cd "/home/$NIX_USER/projects/nixos-config/"
     ;;
   "nixl")
-    cd "/home/$NIX_USER/Documents/NixOS/" || exit
+    cd "/home/$NIX_USER/Documents/NixOS/"
     ;;
   *)
     echo "Error: Folder not recognised. Recognised files are: pro, home, nix/nixr, nixl."
     ;;
   esac
 }
+
 fo "$1"
