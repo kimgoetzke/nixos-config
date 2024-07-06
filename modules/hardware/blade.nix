e8885e308f84b87439135f76e096b7d0be753764
Kim Goetzke <kbgoetzke@gmail.com>
2024-07-06 14:43:56 +0100

    Bump NVidia driver

--- modules/hardware/blade.nix
+++ modules/hardware/blade.nix
@@ -41,7 +41,14 @@
       nvidiaSettings = true;
 
       # Optionally, you may need to select the appropriate driver version for your specific GPU.
-      package = config.boot.kernelPackages.nvidiaPackages.stable;
+      package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+        version = "555.58";
+        sha256_64bit = "sha256-bXvcXkg2kQZuCNKRZM5QoTaTjF4l2TtrsKUvyicj5ew=";
+        sha256_aarch64 = lib.fakeSha256;
+        openSha256 = lib.fakeSha256;
+        settingsSha256 = "sha256-vWnrXlBCb3K5uVkDFmJDVq51wrCoqgPF03lSjZOuU8M=";
+        persistencedSha256 = lib.fakeSha256;
+      };
 
       prime = {
         sync.enable = true;
