324d99cdebf69ae432467b3ad37919b2b19cdc01
Kim Goetzke <kbgoetzke@gmail.com>
2025-01-10 17:44:31 +0000

    Lock before sleep in hyprlock.nix

--- modules/applications/hyprland/hyprlock.nix
+++ modules/applications/hyprland/hyprlock.nix
@@ -17,6 +17,7 @@
         ignore_dbus_inhibit = false;
         ignore_systemd_inhibit = false;
         lock_cmd = "pidof hyprlock || hyprlock";
+        before_sleep_cmd = "loginctl lock-session";
         after_sleep_cmd = "hyprctl dispatch dpms on";
       };
       listener = [
