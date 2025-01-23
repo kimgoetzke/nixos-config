ae1d4540b4eed21edb6fc646f6b08f108603a4e8
Kim Goetzke <kbgoetzke@gmail.com>
2025-01-23 06:31:04 +0000

    Start customising Yazi

--- modules/applications/yazi.nix
+++ modules/applications/yazi.nix
@@ -13,6 +13,13 @@
       enable = true;
       enableZshIntegration = true;
       shellWrapperName = "y";
+      # TODO: Add https://github.com/grappas/wl-clipboard.yazi
+      # plugins = [
+      #   {
+      #     name = "yazi";
+      #     enable = true;
+      #   }
+      # ];
     };
 
     home.sessionVariables = {
@@ -21,6 +28,27 @@
 
     home.file."${userSettings.relativeTargetDirectory}/yazi/yazi.toml" = {
       source = ./../../assets/configs/yazi/yazi.toml;
+    };
+
+    # TODO: Use or remove
+    home.file."${userSettings.relativeTargetDirectory}/yazi/open-with-rofi.sh" = {
+      text = ''
+        #!/bin/bash
+
+        file="$1"
+        if [ ! -f "$file" ]; then
+            notify-send "Error" "File does not exist: $file"
+            exit 1
+        fi
+
+        app=$(rofi -show-icons -show drun)
+        if [ -z "$app" ]; then
+            notify-send "Cancelled" "No application selected"
+            exit 1
+        fi
+
+        "$app" "$file"
+      '';
     };
   };
 }
