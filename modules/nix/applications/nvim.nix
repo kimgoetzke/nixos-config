b7bc4fc24659c0ca3386109d0cb5b5b76e0708bd
Kim Goetzke <kbgoetzke@gmail.com>
2025-01-27 17:31:47 +0000

    Add some initial config for nvim

--- modules/nix/applications/nvim.nix
+++ modules/nix/applications/nvim.nix
@@ -10,23 +10,95 @@ in {
   };
 
   config = lib.mkIf cfg.enable {
-    #programs.neovim = {
-    #  enable = true;
-    #  viAlias = true;
-    #  vimAlias = true;
-    #  vimdiffAlias = true;
-    #  extraPackages = [
-    #    pkgs.shfmt
-    #  ];
-    #};
+    stylix.targets.nixvim.enable = false;
     programs.nixvim = {
       enable = true;
-      colorschemes.nord.enable = true;
+      colorschemes.nord = {
+        enable = true;
+        disableBackground = true;
+        settings = {
+          disable_background = true;
+          borders = true;
+        };
+      };
       viAlias = true;
       vimAlias = true;
+      opts = {
+        number = true;
+        relativenumber = true;
+        shiftwidth = 2;
+        tabstop = 2;
+      };
+      clipboard.providers.wl-copy.enable = true;
 
-      #      lsp.
-      #enableNixIntegration = true;
+      plugins.lsp = {
+        enable = true;
+        servers = {
+          rust_analyzer = {
+            enable = true;
+            installRustc = true;
+            installCargo = true;
+          };
+          lua_ls.enable = true;
+          ts_ls.enable = true;
+          cssls.enable = true;
+          tailwindcss.enable = true;
+          html.enable = true;
+          nil_ls.enable = true;
+          dockerls.enable = true;
+          bashls.enable = true;
+          yamlls.enable = true;
+        };
+      };
+
+      plugins.cmp = {
+        enable = true;
+        autoEnableSources = true;
+        settings = {
+          sources = [
+            {name = "path";}
+            {name = "nvim_lsp";}
+            {name = "luasnip";}
+            {
+              name = "buffer";
+              option.get_bufnrs.__raw = "vim.api.nvim_list_bufs";
+            }
+            {name = "neorg";}
+          ];
+        };
+      };
+
+      plugins.markdown-preview = {
+        enable = true;
+        settings = {
+          auto_close = 0;
+          theme = "dark";
+        };
+      };
+
+      plugins.image = {
+        enable = true;
+        integrations = {
+          markdown = {
+            enabled = true;
+            downloadRemoteImages = true;
+          };
+        };
+      };
+
+      plugins.telescope = {
+        enable = true;
+      };
+
+      plugins.treesitter.enable = true;
+      plugins.luasnip.enable = true;
+      plugins.web-devicons.enable = true;
+
+      plugins.colorizer = {
+        enable = true;
+        settings.user_default_options.names = false;
+        settings.filetypes = ["css" "scss"];
+      };
     };
   };
 }
