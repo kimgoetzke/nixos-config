cf080d5376edd12710fca72c2e8a6009f90b12b8
Kim Goetzke <kbgoetzke@gmail.com>
2026-05-17 06:52:47 +0100

    Add Chromium for a Pi extension

--- modules/nix/applications/ai.nix
+++ modules/nix/applications/ai.nix
@@ -15,10 +15,12 @@ in {
       github-copilot-cli # Will also install Node.js
       claude-code
       pi-coding-agent
+      chromium # Optional improvement for https://github.com/kimgoetzke/coding-agent-configs/tree/main/.pi/agent/extensions/web-tools
     ];
 
     environment.sessionVariables = {
       PI_SKIP_VERSION_CHECK = "1";
+      PLAYWRIGHT_CHROMIUM_EXECUTABLE_PATH = lib.getExe pkgs.chromium;
     };
   };
 }
